1c255a1c35b5433e8360b7a9a44fc2382ad30acd
Karen Guerrero <Katthyren@hotmail.com>
2021-11-24 11:29:17 +0000

    Using protocol methods

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+import sys
+import os
+from math import pi
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class Circle:
+    def __init__(self, radius, fill='red', stroke='black'):
+        self._radius = radius # Private/protected
+        self._fill = fill
+        self._stroke = stroke
+    @property
+    def radius(self): #public access for radius READ ONLY
+        return self._radius
+    @property
+    def are(self):
+        return self.calculate_area()
+    @property  # decorator
+    def diameter(self):
+        return self._radius*2
+
+    def calculate_area(self):
+        """
+        Calculare the area
+        :return:
+        """
+        return pi * self._radius ** 2
+    def __len__(self):
+        return int(2 * pi *self._radius) # using the private value
+
+    def __call__(self):
+        return "I am a happy circle c:"
+
+def main():
+    circle = Circle(5.0, fill='orange', stroke='red')
+    print(f"Area = {circle.calculate_area()}")
+    circle2 = Circle(8.0) # default values
+    print(f"area = {circle.are}")
+    print(f"circunference is {len(circle)}")
+    print(circle._radius)
+    print(circle())
+    return 0
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    sys.exit(main())
